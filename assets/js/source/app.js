cc15b5b20894c0a8accc9ad5fabed78044f4d1e8
Rafael Macedo <rmacedo@godaddy.com>
2016-07-11 11:49:55 -0700

    Site updated

--- assets/js/source/app.js
+++
@@ -1,9 +0,0 @@
-import countdown from 'countdown';
-
-var timerId = countdown(
-  new Date(2016, 8, 23),
-  function(ts) {
-    document.getElementById('timer').innerHTML = ts.toHTML("strong");
-  },
-  countdown.ALL
-);
